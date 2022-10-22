db16c103cfceb21eb8ca98abb4354e78f74d6c6c
lakshyana <lakshyana.kc@nyu.edu>
2022-10-22 03:10:18 -0400

    minor edit

--- public/tuiter/explore/HomeScreen/index.js
+++ public/tuiter/explore/HomeScreen/index.js
@@ -1,9 +1,7 @@
 
 import NavigationSidebar from "../NavigationSidebar/index.js";
-// import PostSummaryList from "../PostSummaryList/index.js";
 import PostList from "../PostList/index.js";
-
-import PostSummaryList from "../PostSummaryList/index";
+import PostSummaryList from "../PostSummaryList/index.js";
 
 
 function homeScreen() {
