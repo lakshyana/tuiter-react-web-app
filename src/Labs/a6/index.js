617c6ffb7cc377cb5f41311dc5d3e462c5d1fbec
lakshyanakc <lakshyana@buildchange.org>
2022-10-25 23:12:58 -0400

    a6

--- src/Labs/a6/index.js
+++ src/Labs/a6/index.js
@@ -2,17 +2,17 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput
     from "./conditional-output";
-// import TodoList from "./todo/TodoList";
-import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
+// import TodoItem from "./todo/TodoItem";
 
 function Assignment6() {
     return (
         <div>
             <h1>Assignment 6</h1>
-            <TodoItem/>
             <ConditionalOutput/>
             <Styles/>
             <Classes/>
+            <TodoList/>
         </div>
     );
 }
