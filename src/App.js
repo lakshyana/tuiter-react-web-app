bc568b1a355563d51435f71acf81b5917dffbba3
lakshyanakc <lakshyana@buildchange.org>
2022-10-25 22:43:14 -0400

    as

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Labs from "./Labs";
+import HelloWorld from "./Labs/hello-world/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Welcome to Web Development Fall 2022
-        </h1>
-        <h2>
-          This is a branch for assignment 2
-        </h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route index ="/labs"
+                           element={<Labs/>}/>
+                    <Route path="/hello"
+                           element={<HelloWorld/>}/>
+                    <Route path="/tuiter"
+                           element={<Tuiter/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
-
 export default App;
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>
+//           Welcome to Web Development Fall 2022
+//         </h1>
+//         <h2>
+//           This is a branch for assignment 2
+//         </h2>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
---
+++ src/Labs/a6/index.js
@@ -0,0 +1,19 @@
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput
+    from "./conditional-output";
+// import TodoList from "./todo/TodoList";
+import TodoItem from "./todo/TodoItem";
+
+function Assignment6() {
+    return (
+        <div>
+            <h1>Assignment 6</h1>
+            <TodoItem/>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+        </div>
+    );
+}
+export default Assignment6;
