6ae37f460c60a11ae177ac369f19970712c6e74e
lakshyanakc <lakshyana@buildchange.org>
2022-11-22 16:35:12 -0500

    nav link test

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -38,7 +38,7 @@ function Tuiter() {
         <Provider store={store}>
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar selected={"home"}/>
+                <NavigationSidebar selected="home"/>
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6">
                 <Routes>
